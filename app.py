3f26da8b6d22d2f517654e42f9eca34897bb75f3
workingforyou728 <workingforyou728@gmail.com>
2025-11-26 16:49:28 +0530

    Update REDIRECT_URL to new
    websitewebsite update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 # Set your redirect link here
-REDIRECT_URL = "https://your-website.com"   # <-- change after 3 days
+REDIRECT_URL = "https://inspireoneeducationalservices.com/index.html"   # <-- change after 3 days
 
 @app.route("/")
 def home():
